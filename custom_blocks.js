064aa0f6fa2a41112f60d1b9d9ce8cf9257911e4
stand4rd <thomas.noble.jordan@gmail.com>
2023-10-01 22:11:10 -0700

    added test functionality for custom blocks

--- custom_blocks.js
+++ custom_blocks.js
@@ -1,1 +1,18 @@
-
+Blockly.Blocks['printTest'] = {
+    init: function() {
+        this.appendDummyInput()
+        .appendField("Print 'test' to screen");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(160);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+    
+Blockly.JavaScript['printTest'] = function(block) {
+    // Generate JavaScript code to print "test" to the screen
+    var code = 'console.log("test");\n';
+    return code;
+};
+    
